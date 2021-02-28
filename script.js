7e7f8f13f5aaa6c14b76f9cd24343f95edfc999b
Paulo <hst.paulo@gmail.com>
2021-02-28 13:04:59 -0300

    pequenas correcoes no estilo

---
+++ script.js
@@ -0,0 +1,10 @@
+const projects = [
+    'http://placehold.it/250x250',
+    'http://placehold.it/250x250',
+    'http://placehold.it/250x250',
+    'http://placehold.it/250x250',
+    'http://placehold.it/250x250',
+    'http://placehold.it/250x250',
+    'http://placehold.it/250x250',
+]
+
